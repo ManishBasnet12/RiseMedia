985fe388444cece2356803528557c79feb43e0cd
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2023-09-27 13:28:55 +0545

    first push

---
+++ JS/footer.js
@@ -0,0 +1,119 @@
+// var prevScrollpos = window.pageYOffset;
+// var headerNavigation = document.getElementById("headernavigation");
+// var scrollThreshold = 0;
+// var isHeaderVisible = true;
+
+// function throttle(callback, delay) {
+//     var timeoutId;
+//     return function () {
+//         if (!timeoutId) {
+//             timeoutId = setTimeout(function () {
+//                 callback();
+//                 timeoutId = null;
+//             }, delay);
+//         }
+//     };
+// }
+
+// var throttledScroll = throttle(function () {
+//     var currentScrollPos = window.pageYOffset;
+
+//     if (currentScrollPos > scrollThreshold) {
+//         if (prevScrollpos > currentScrollPos) {
+//             if (!isHeaderVisible) {
+//                 animateHeader("0px");
+//                 isHeaderVisible = true;
+//             }
+//         } else {
+//             if (isHeaderVisible) {
+//                 animateHeader("-100px");
+//                 isHeaderVisible = false;
+//             }
+//         }
+//     } else {
+//         headerNavigation.style.position = "absolute";
+//         headerNavigation.style.top = "0";
+//     }
+
+//     prevScrollpos = currentScrollPos;
+// }, 100); 
+
+// window.addEventListener("scroll", throttledScroll);
+
+// function animateHeader(topValue) {
+//     headerNavigation.style.transition = "top 0.3s";
+//     headerNavigation.style.position = "fixed";
+//     headerNavigation.style.top = topValue;
+// }
+
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("headernavigation").style.top = "0";
+//     } else {
+//         document.getElementById("headernavigation").style.top = "-100px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+
+
+
+// Navigation scroll
+window.addEventListener('scroll', function (e) {
+	reloadNavigationBarIfNeeded();
+});
+
+function topOffset(percentage) {
+	if (percentage >= 1) {
+		return "0 px";
+	}
+
+	const offset = Math.round((percentage - 0.2) * 200)
+
+	return offset + "px";
+}
+
+function scrollPercentage(element, strict) {
+	const viewportHeight = window.innerHeight;
+	const scrollTop = window.scrollY;
+	const elementOffsetTop = element.offsetTop;
+	const elementHeight = element.offsetHeight;
+
+	const offset = (scrollTop - elementOffsetTop)
+
+	if (strict) {
+		return Math.min(1, Math.max((offset / elementHeight), 0))
+	} else {
+		return offset / elementHeight
+	}
+}
+
+function reloadNavigationBarIfNeeded() {
+	navigation = document.getElementById("mainheader");
+	targetContainer = document.querySelector(".nav-toggle-target");
+
+	const targetOffset = targetContainer.getBoundingClientRect().top;
+	const offset = window.pageYOffset;
+
+	if (targetOffset >= 220) {
+		if (!navigation.classList.contains('minimal')) {
+			navigation.classList.add('minimal');
+		}
+	} else {
+		if (navigation.classList.contains('minimal')) {
+			navigation.classList.remove('minimal');
+		}
+	}
+}
+
+window.onhashchange = function () {
+	if (location.hash == "#dropover-cloud") {
+		document.title = "Dropover Cloud - Instantly save your dragged content to the cloud"
+	} else {
+		document.title = "Dropover - Easier Drag and Drop on your Mac.";
+	}
+}
+;
---
+++ JS/navigation.js
@@ -0,0 +1,172 @@
+document.querySelector(".mainheader").innerHTML=` <div class="header-top " style="display: none;">
+<div class="container">
+    <div class="row">
+        <div class="left">
+            <div class="image">
+                <img src="/IMAGE/rise.png" alt="">
+            </div>
+            <div class="search">
+                <form action="">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <input type="search" placeholder="Search">
+                    <button hidden></button>
+                </form>
+            </div>
+        </div>
+        <div class="right">
+            <div class="phone contact">
+                <div class="icon">
+                    <i class="ri-phone-fill"></i>
+                </div>
+                <div class="detail">
+                    <h3>Phone</h3>
+                    <p>123456789</p>
+                </div>
+            </div>
+            <div class="Email contact">
+                <div class="icon">
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <div class="detail">
+                    <h3>E-mail</h3>
+                    <p>ABC@email.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+<!-- <div class="header-bottom py-1"
+style="position: absolute; top: 0; right: 0; left: 0; z-index: 1; background-color: rgba(0, 0, 0, 0.4); filter: blur(30%);">
+<div class="container">
+    <div class="row">
+        <div class="mail-logo">
+            <img src="/IMAGE/rise.png" alt="">
+        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="127.0.0.1:5500/HTML/index.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="127.0.0.1:5500/HTML/services.html">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Our Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="services.html">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="creation.html">Creation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Gallery</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Advertisment</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                </div>
+            </div>
+        </nav>
+        <div class="right ">
+            <div class="phone contact">
+                <div class="icon">
+                    <i class="fa-solid fa-phone"></i>
+                </div>
+                <div class="detail">
+                    <h3>Phone</h3>
+                    <p>123456789</p>
+                </div>
+            </div>
+            <div class="Email contact">
+                <div class="icon">
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <div class="detail">
+                    <h3>E-mail</h3>
+                    <p>ABC@email.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div> -->
+<div class="header-bottom py-1" id="headernavigation" style=" background-color: white; backdrop-filter:blur(5px);">
+<div class="container">
+    <div class="row">
+        <div class="mail-logo nav-logo">
+            <a href="http://127.0.0.1:5500/HTML/"> <img src="/IMAGE/rise.png" alt=""></a>
+        </div>
+        <div class="mail-logo body-logo" >
+            <a href="http://127.0.0.1:5500/HTML/"> <img src="/IMAGE/rise2.png" alt=""></a>
+        </div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="http://127.0.0.1:5500/HTML/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:5500/HTML/aboutus.html">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:5500/HTML/team.html">Our Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:5500/HTML/services.html">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:5500/HTML/creation.html">Creation</a>
+                        </li>
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#">Gallery</a>
+                        </li> -->
+                        <!-- <li class="nav-item">
+                            <a class="nav-link" href="#">Advertisment</a>
+                        </li> -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="http://127.0.0.1:5500/HTML/contact.html">Contact Us</a>
+                        </li>
+                </div>
+            </div>
+        </nav>
+        <div class="right ">
+            <div class="phone contact">
+                <div class="icon">
+                    <i class="fa-solid fa-phone"></i>
+                </div>
+                <div class="detail">
+                    <h3>Phone</h3>
+                    <p>123456789</p>
+                </div>
+            </div>
+            <div class="Email contact">
+                <div class="icon">
+                    <i class="fa-solid fa-envelope"></i>
+                </div>
+                <div class="detail">
+                    <h3>E-mail</h3>
+                    <p>ABC@email.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`
