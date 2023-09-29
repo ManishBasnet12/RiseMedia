38efa2918ee17799dbc5ecb67e822136557c91da
basnetkazi64@gmail.com <basnetkazi64@gmail.com>
2023-09-29 14:53:46 +0545

    page

--- JS/navigation.js
+++ JS/navigation.js
@@ -1,116 +1,12 @@
 document.querySelector(".mainheader").innerHTML=` 
-<div class="header-top " style="display: none;">
-<div class="container">
-    <div class="row">
-        <div class="left">
-            <div class="image">
-                <img src="/IMAGE/rise.png" alt="">
-            </div>
-            <div class="search">
-                <form action="">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <input type="search" placeholder="Search">
-                    <button hidden></button>
-                </form>
-            </div>
-        </div>
-        <div class="right">
-            <div class="phone contact">
-                <div class="icon">
-                    <i class="ri-phone-fill"></i>
-                </div>
-                <div class="detail">
-                    <h3>Phone</h3>
-                    <p>123456789</p>
-                </div>
-            </div>
-            <div class="Email contact">
-                <div class="icon">
-                    <i class="fa-solid fa-envelope"></i>
-                </div>
-                <div class="detail">
-                    <h3>E-mail</h3>
-                    <p>ABC@email.com</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div>
-<!-- <div class="header-bottom py-1"
-style="position: absolute; top: 0; right: 0; left: 0; z-index: 1; background-color: rgba(0, 0, 0, 0.4); filter: blur(30%);">
-<div class="container">
-    <div class="row">
-        <div class="mail-logo">
-            <img src="/IMAGE/rise.png" alt="">
-        </div>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="127.0.0.1:5500/HTML/index.html">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="127.0.0.1:5500/HTML/services.html">About Us</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Our Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="services.html">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="creation.html">Creation</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Gallery</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Advertisment</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact Us</a>
-                        </li>
-                </div>
-            </div>
-        </nav>
-        <div class="right ">
-            <div class="phone contact">
-                <div class="icon">
-                    <i class="fa-solid fa-phone"></i>
-                </div>
-                <div class="detail">
-                    <h3>Phone</h3>
-                    <p>123456789</p>
-                </div>
-            </div>
-            <div class="Email contact">
-                <div class="icon">
-                    <i class="fa-solid fa-envelope"></i>
-                </div>
-                <div class="detail">
-                    <h3>E-mail</h3>
-                    <p>ABC@email.com</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</div> -->
 <div class="header-bottom py-1" id="headernavigation" style=" background-color: white; backdrop-filter:blur(5px);">
-<div class="container">
+<div class="container-fluid">
     <div class="row">
         <div class="mail-logo nav-logo">
-            <a href="https://risemedianetwork.netlify.app"> <img src="/IMAGE/rise.png" alt=""></a>
+            <a href="http://127.0.0.1:5500/"> <img src="/IMAGE/rise.png" alt=""></a>
         </div>
         <div class="mail-logo body-logo" >
-            <a href="https://risemedianetwork.netlify.app"> <img src="/IMAGE/rise2.png" alt=""></a>
+            <a href="http://127.0.0.1:5500/"> <img src="/IMAGE/rise2.png" alt=""></a>
         </div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
@@ -122,19 +18,19 @@ style="position: absolute; top: 0; right: 0; left: 0; z-index: 1; background-col
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="https://risemedianetwork.netlify.app">Home</a>
+                            <a class="nav-link active" aria-current="page" href="http://127.0.0.1:5500/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://risemedianetwork.netlify.app/aboutus">About Us</a>
+                            <a class="nav-link" href="http://127.0.0.1:5500/aboutus.html">About Us</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://risemedianetwork.netlify.app/team">Our Team</a>
+                            <a class="nav-link" href="http://127.0.0.1:5500/team.html">Our Team</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://risemedianetwork.netlify.app/services">Services</a>
+                            <a class="nav-link" href="http://127.0.0.1:5500/services.html">Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="https://risemedianetwork.netlify.app/creation">Creation</a>
+                            <a class="nav-link" href="http://127.0.0.1:5500/creation.html">Creation</a>
                         </li>
                         <!-- <li class="nav-item">
                             <a class="nav-link" href="#">Gallery</a>
@@ -143,7 +39,7 @@ style="position: absolute; top: 0; right: 0; left: 0; z-index: 1; background-col
                             <a class="nav-link" href="#">Advertisment</a>
                         </li> -->
                         <li class="nav-item">
-                            <a class="nav-link" href="https://risemedianetwork.netlify.app/contact">Contact Us</a>
+                            <a class="nav-link" href="http://127.0.0.1:5500/contact.html">Contact Us</a>
                         </li>
                 </div>
             </div>
